c3518efb3f69d1a56a04e4c9acd277b4eec789fa
hai-quan <hai-quan@system-exe.com.vn>
2024-09-30 13:21:04 +0700

    script create DB

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -1,196 +1,86 @@
-create database eazybank;
+CREATE DATABASE projectmovie
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
-use eazybank;
-
-drop table `users`;
-drop table `authorities`;
-drop table `customer`;
-
-CREATE TABLE `customer` (
-  `customer_id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `m_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `mobile_number` varchar(20) NOT NULL,
   `pwd` varchar(500) NOT NULL,
   `role` varchar(100) NOT NULL,
-  `create_dt` date DEFAULT NULL,
-  PRIMARY KEY (`customer_id`)
-);
-
-INSERT INTO `customer` (`name`,`email`,`mobile_number`, `pwd`, `role`,`create_dt`)
- VALUES ('Happy','happy@example.com','9876548337', '$2y$12$oRRbkNfwuR8ug4MlzH5FOeui.//1mkd.RsOAJMbykTSupVy.x/vb2', 'admin',CURDATE());
-
-CREATE TABLE `accounts` (
-  `customer_id` int NOT NULL,
-   `account_number` int NOT NULL,
-  `account_type` varchar(100) NOT NULL,
-  `branch_address` varchar(200) NOT NULL,
-  `create_dt` date DEFAULT NULL,
-  PRIMARY KEY (`account_number`),
-  KEY `customer_id` (`customer_id`),
-  CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
-);
-
-INSERT INTO `accounts` (`customer_id`, `account_number`, `account_type`, `branch_address`, `create_dt`)
- VALUES (1, 1865764534, 'Savings', '123 Main Street, New York', CURDATE());
-
-CREATE TABLE `account_transactions` (
-  `transaction_id` varchar(200) NOT NULL,
-  `account_number` int NOT NULL,
-  `customer_id` int NOT NULL,
-  `transaction_dt` date NOT NULL,
-  `transaction_summary` varchar(200) NOT NULL,
-  `transaction_type` varchar(100) NOT NULL,
-  `transaction_amt` int NOT NULL,
-  `closing_balance` int NOT NULL,
-  `create_dt` date DEFAULT NULL,
-  PRIMARY KEY (`transaction_id`),
-  KEY `customer_id` (`customer_id`),
-  KEY `account_number` (`account_number`),
-  CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`account_number`) REFERENCES `accounts` (`account_number`) ON DELETE CASCADE,
-  CONSTRAINT `acct_user_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
-);
-
-
-
-INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
-`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, DATE_SUB(CURDATE(), INTERVAL 7 DAY), 'Coffee Shop', 'Withdrawal', 30,34500,DATE_SUB(CURDATE(), INTERVAL 7 DAY));
-
-INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
-`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, DATE_SUB(CURDATE(), INTERVAL 6 DAY), 'Uber', 'Withdrawal', 100,34400,DATE_SUB(CURDATE(), INTERVAL 6 DAY));
-
-INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
-`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, DATE_SUB(CURDATE(), INTERVAL 5 DAY), 'Self Deposit', 'Deposit', 500,34900,DATE_SUB(CURDATE(), INTERVAL 5 DAY));
-
-INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
-`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, DATE_SUB(CURDATE(), INTERVAL 4 DAY), 'Ebay', 'Withdrawal', 600,34300,DATE_SUB(CURDATE(), INTERVAL 4 DAY));
-
-INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
-`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, DATE_SUB(CURDATE(), INTERVAL 2 DAY), 'OnlineTransfer', 'Deposit', 700,35000,DATE_SUB(CURDATE(), INTERVAL 2 DAY));
-
-INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
-`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, DATE_SUB(CURDATE(), INTERVAL 1 DAY), 'Amazon.com', 'Withdrawal', 100,34900,DATE_SUB(CURDATE(), INTERVAL 1 DAY));
-
-
-CREATE TABLE `loans` (
-  `loan_number` int NOT NULL AUTO_INCREMENT,
-  `customer_id` int NOT NULL,
-  `start_dt` date NOT NULL,
-  `loan_type` varchar(100) NOT NULL,
-  `total_loan` int NOT NULL,
-  `amount_paid` int NOT NULL,
-  `outstanding_amount` int NOT NULL,
-  `create_dt` date DEFAULT NULL,
-  PRIMARY KEY (`loan_number`),
-  KEY `customer_id` (`customer_id`),
-  CONSTRAINT `loan_customer_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
-);
-
-INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2020-10-13', 'Home', 200000, 50000, 150000, '2020-10-13');
-
-INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2020-06-06', 'Vehicle', 40000, 10000, 30000, '2020-06-06');
-
-INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2018-02-14', 'Home', 50000, 10000, 40000, '2018-02-14');
-
-INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2018-02-14', 'Personal', 10000, 3500, 6500, '2018-02-14');
-
-CREATE TABLE `cards` (
-  `card_id` int NOT NULL AUTO_INCREMENT,
-  `card_number` varchar(100) NOT NULL,
-  `customer_id` int NOT NULL,
-  `card_type` varchar(100) NOT NULL,
-  `total_limit` int NOT NULL,
-  `amount_used` int NOT NULL,
-  `available_amount` int NOT NULL,
-  `create_dt` date DEFAULT NULL,
-  PRIMARY KEY (`card_id`),
-  KEY `customer_id` (`customer_id`),
-  CONSTRAINT `card_customer_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
-);
-
-INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, `amount_used`, `available_amount`, `create_dt`)
- VALUES ('4565XXXX4656', 1, 'Credit', 10000, 500, 9500, CURDATE());
-
-INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, `amount_used`, `available_amount`, `create_dt`)
- VALUES ('3455XXXX8673', 1, 'Credit', 7500, 600, 6900, CURDATE());
-
-INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, `amount_used`, `available_amount`, `create_dt`)
- VALUES ('2359XXXX9346', 1, 'Credit', 20000, 4000, 16000, CURDATE());
-
-CREATE TABLE `notice_details` (
-  `notice_id` int NOT NULL AUTO_INCREMENT,
-  `notice_summary` varchar(200) NOT NULL,
-  `notice_details` varchar(500) NOT NULL,
-  `notic_beg_dt` date NOT NULL,
-  `notic_end_dt` date DEFAULT NULL,
-  `create_dt` date DEFAULT NULL,
-  `update_dt` date DEFAULT NULL,
-  PRIMARY KEY (`notice_id`)
-);
-
-INSERT INTO `notice_details` ( `notice_summary`, `notice_details`, `notic_beg_dt`, `notic_end_dt`, `create_dt`, `update_dt`)
-VALUES ('Home Loan Interest rates reduced', 'Home loan interest rates are reduced as per the goverment guidelines. The updated rates will be effective immediately',
-CURDATE() - INTERVAL 30 DAY, CURDATE() + INTERVAL 30 DAY, CURDATE(), null);
-
-INSERT INTO `notice_details` ( `notice_summary`, `notice_details`, `notic_beg_dt`, `notic_end_dt`, `create_dt`, `update_dt`)
-VALUES ('Net Banking Offers', 'Customers who will opt for Internet banking while opening a saving account will get a $50 amazon voucher',
-CURDATE() - INTERVAL 30 DAY, CURDATE() + INTERVAL 30 DAY, CURDATE(), null);
-
-INSERT INTO `notice_details` ( `notice_summary`, `notice_details`, `notic_beg_dt`, `notic_end_dt`, `create_dt`, `update_dt`)
-VALUES ('Mobile App Downtime', 'The mobile application of the EazyBank will be down from 2AM-5AM on 12/05/2020 due to maintenance activities',
-CURDATE() - INTERVAL 30 DAY, CURDATE() + INTERVAL 30 DAY, CURDATE(), null);
-
-INSERT INTO `notice_details` ( `notice_summary`, `notice_details`, `notic_beg_dt`, `notic_end_dt`, `create_dt`, `update_dt`)
-VALUES ('E Auction notice', 'There will be a e-auction on 12/08/2020 on the Bank website for all the stubborn arrears.Interested parties can participate in the e-auction',
-CURDATE() - INTERVAL 30 DAY, CURDATE() + INTERVAL 30 DAY, CURDATE(), null);
-
-INSERT INTO `notice_details` ( `notice_summary`, `notice_details`, `notic_beg_dt`, `notic_end_dt`, `create_dt`, `update_dt`)
-VALUES ('Launch of Millennia Cards', 'Millennia Credit Cards are launched for the premium customers of EazyBank. With these cards, you will get 5% cashback for each purchase',
-CURDATE() - INTERVAL 30 DAY, CURDATE() + INTERVAL 30 DAY, CURDATE(), null);
-
-INSERT INTO `notice_details` ( `notice_summary`, `notice_details`, `notic_beg_dt`, `notic_end_dt`, `create_dt`, `update_dt`)
-VALUES ('COVID-19 Insurance', 'EazyBank launched an insurance policy which will cover COVID-19 expenses. Please reach out to the branch for more details',
-CURDATE() - INTERVAL 30 DAY, CURDATE() + INTERVAL 30 DAY, CURDATE(), null);
-
-CREATE TABLE `contact_messages` (
-  `contact_id` varchar(50) NOT NULL,
-  `contact_name` varchar(50) NOT NULL,
-  `contact_email` varchar(100) NOT NULL,
-  `subject` varchar(500) NOT NULL,
-  `message` varchar(2000) NOT NULL,
-  `create_dt` date DEFAULT NULL,
-  PRIMARY KEY (`contact_id`)
-);
+  `is_enable` bit(1) NOT NULL,
+  `valid_flag` bit(1) DEFAULT NULL,
+  `create_dt` datetime DEFAULT NULL,
+  `create_by` varchar(100) DEFAULT NULL,
+  `update_dt` datetime DEFAULT NULL,
+  `update_by` varchar(100) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `authorities` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `customer_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `customer_id` (`customer_id`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
-);
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `m_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO `authorities` (`customer_id`, `name`)
- VALUES (1, 'VIEWACCOUNT');
+CREATE TABLE `m_codes` (
+  `code_type` varchar(10) NOT NULL,
+  `code_type_name` varchar(150) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  `code_name` varchar(45) NOT NULL,
+  `valid_flag` bit(1) DEFAULT NULL,
+  `create_dt` datetime DEFAULT NULL,
+  `create_by` varchar(200) DEFAULT NULL,
+  `update_dt` datetime DEFAULT NULL,
+  `update_by` varchar(200) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`code_type`,`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `authorities` (`customer_id`, `name`)
- VALUES (1, 'VIEWCARDS');
+CREATE TABLE `m_movie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `movie_name` varchar(100) NOT NULL,
+  `country` varchar(10) DEFAULT NULL,
+  `release_year` varchar(10) DEFAULT NULL,
+  `genre` varchar(10) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `image` blob,
+  `rating` decimal(1,1) DEFAULT NULL,
+  `is_show` bit(1) DEFAULT b'1',
+  `valid_flag` bit(1) DEFAULT NULL,
+  `create_dt` datetime DEFAULT NULL,
+  `create_by` varchar(100) DEFAULT NULL,
+  `update_dt` datetime DEFAULT NULL,
+  `update_by` varchar(100) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
- INSERT INTO `authorities` (`customer_id`, `name`)
-  VALUES (1, 'VIEWLOANS');
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','1','United States','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','10','Russia','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','11','Vietnam','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','12','South Korea','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','13','Italy','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','14','Spain','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','15','Mexico','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','2','Canada','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','3','United Kingdom','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','4','Australia','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','5','Germany','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','6','France','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','7','Japan','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','8','China','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
+INSERT INTO `m_codes` (`code_type`,`code_type_name`,`code`,`code_name`,`is_show`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES ('000001','country','9','India','1','1','2024-08-29 13:46:03','admin','2024-08-29 13:46:03','admin',1);
 
- INSERT INTO `authorities` (`customer_id`, `name`)
-   VALUES (1, 'VIEWBALANCE');
+INSERT INTO `m_user` (`user_id`,`name`,`email`,`pwd`,`role`,`is_enable`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES (1,'Administrator','happy@example.com','$2a$12$x2fLA21B4QWphy9D8OWO1.Ny7X9wjDI30eXhTMSx2dOXTbrjtQNQK','admin','1','1','2024-08-26 00:00:00','admin','2024-08-29 08:59:27','happy@example.com',1);
+INSERT INTO `m_user` (`user_id`,`name`,`email`,`pwd`,`role`,`is_enable`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES (2,'tanya266@gmail.com','tanya266@gmail.com','$2a$10$rjwRoUhplz2DYWapy4kzHuGElDZWUP9DNex3TFAISKYxixRNqtHwC','user','1','1',NULL,NULL,NULL,NULL,1);
+INSERT INTO `m_user` (`user_id`,`name`,`email`,`pwd`,`role`,`is_enable`,`valid_flag`,`create_dt`,`create_by`,`update_dt`,`update_by`,`version`) VALUES (3,'tranhaiquan26@gmail.com','tranhaiquan26@gmail.com','$2a$10$Q4DI1EUafO2wwbDYixc5CuqGOgGJ1o17vR9AMtAQNueE7WjdFk1OC','user','1','1','2024-08-28 09:29:05','admin','2024-08-29 13:04:47','tranhaiquan26@gmail.com',1);
 
- DELETE FROM `authorities`;
-
- INSERT INTO `authorities` (`customer_id`, `name`)
-  VALUES (1, 'ROLE_USER');
-
- INSERT INTO `authorities` (`customer_id`, `name`)
-  VALUES (1, 'ROLE_ADMIN');
+INSERT INTO `authorities` (`id`,`user_id`,`name`) VALUES (1,1,'ROLE_USER');
+INSERT INTO `authorities` (`id`,`user_id`,`name`) VALUES (2,1,'ROLE_ADMIN');
+INSERT INTO `authorities` (`id`,`user_id`,`name`) VALUES (3,2,'ROLE_USER');
+INSERT INTO `authorities` (`id`,`user_id`,`name`) VALUES (4,3,'ROLE_USER');
